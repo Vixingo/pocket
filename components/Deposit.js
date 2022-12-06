5656a494df536a34b934682676faa2677bc36bcf
Nazmul <Nazmul.abbu@gmail.com>
2022-12-06 19:07:01 +0600

    deposit complete

--- components/Deposit.js
+++ components/Deposit.js
@@ -6,11 +6,19 @@ import {
     Stack,
     Typography,
 } from "@mui/material";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
 import React from "react";
 import CreditCardRoundedIcon from "@mui/icons-material/CreditCardRounded";
 import Image from "next/image";
 import NavFooter from "./NavFooter";
-
+import HelpOutlineRoundedIcon from "@mui/icons-material/HelpOutlineRounded";
+import CheckRoundedIcon from "@mui/icons-material/CheckRounded";
+import AccountBalanceIcon from "@mui/icons-material/AccountBalance";
+import LocalAtmRoundedIcon from "@mui/icons-material/LocalAtmRounded";
+import PercentIcon from "@mui/icons-material/Percent";
+import HourglassEmptyIcon from "@mui/icons-material/HourglassEmpty";
+import CreditCardIcon from "@mui/icons-material/CreditCard";
 function D_box() {
     return (
         <Box className="col-lg-6 col-md-4 col-sm-6 ">
@@ -54,7 +62,16 @@ function Deposit() {
                     width: "100%",
                 }}
             >
-                <Box className="row" sx={{ padding: "20px" }}>
+                <Box
+                    className="row"
+                    sx={{
+                        padding: "100px",
+                        paddingTop: "50px",
+                        "@media(max-width:600px)": {
+                            padding: "20px",
+                        },
+                    }}
+                >
                     <Box className="col-lg-6 ">
                         <Typography
                             sx={{
@@ -83,16 +100,19 @@ function Deposit() {
                             <D_box />
                             <D_box />
                             <D_box />
-                            <D_box />
-                            <D_box />
-                            <D_box />
-                            <D_box />
-                            <D_box />
-                            <D_box />
-                            <D_box />
-                            <D_box />
-                            <D_box />
-                            <D_box />
+                        </Box>
+                        <Divider
+                            sx={{
+                                margin: "10px auto",
+                                color: "#fff",
+                            }}
+                        >
+                            <Typography sx={{ display: "flex" }}>
+                                {" "}
+                                <LocalAtmRoundedIcon /> &nbsp; E-payments
+                            </Typography>
+                        </Divider>
+                        <Box className="row">
                             <D_box />
                             <D_box />
                             <D_box />
@@ -106,8 +126,106 @@ function Deposit() {
                     </Box>
                     <Box className="col-lg-6">
                         <div className="row">
-                            <div className="col-md-6"> Heloo</div>
-                            <div className="col-md-6"> gelo</div>
+                            <div className="col-md-6">
+                                {" "}
+                                <Typography
+                                    sx={{
+                                        color: "#fff",
+                                        marginBottom: "25px",
+                                        fontWeight: "400",
+                                    }}
+                                >
+                                    Your profile level{" "}
+                                    <Tooltip
+                                        title="Profile level affects your Social Trading status and reflects your loyalty to Pocket Option. The higher your Profile level, the more various offers you will receive from the company. This is the company's initiative to help you achieve better trading performance. For our clients with high levels, we open access to premium services and attractive products in the Market."
+                                        arrow
+                                        placement="top"
+                                    >
+                                        <HelpOutlineRoundedIcon fontSize="14px" />
+                                    </Tooltip>
+                                </Typography>
+                            </div>
+                            <div className="col-md-12">
+                                <Box className="user_level_block user_st">
+                                    <CheckRoundedIcon
+                                        sx={{
+                                            fontWeight: "700",
+                                            fontSize: "20px",
+                                            left: "7px",
+                                            position: "absolute",
+                                        }}
+                                    />{" "}
+                                    Stranger [Without deposit]
+                                </Box>
+                                <Box className="user_level_block user_nb">
+                                    Newbie [Balance less than $100]{" "}
+                                </Box>
+                                <Box className="user_level_block user_beg">
+                                    Beginner [Min balance: $100]{" "}
+                                </Box>
+                                <Box className="user_level_block user_ex">
+                                    Experienced [Min balance: $1000]{" "}
+                                </Box>
+                                <Box className="user_level_block user_ms">
+                                    Master [Min balance: $5000]
+                                </Box>
+                                <Box className="user_level_block user_pro">
+                                    Pro [Min balance: $15000]
+                                </Box>
+                                <Box className="user_level_block user_guru">
+                                    Guru [Min balance: $50000]
+                                </Box>
+                                <Box className="reqire">
+                                    <Typography
+                                        sx={{
+                                            color: "#fff",
+                                            fontSize: "13px",
+                                            marginBottom: "5px",
+                                        }}
+                                    >
+                                        <AccountBalanceIcon
+                                            sx={{ color: "#009eff" }}
+                                        />{" "}
+                                        Minimum deposit amount: $5
+                                    </Typography>
+                                    <Typography
+                                        sx={{
+                                            color: "#fff",
+                                            fontSize: "13px",
+                                            marginBottom: "5px",
+                                        }}
+                                    >
+                                        <PercentIcon
+                                            sx={{ color: "#009eff" }}
+                                        />{" "}
+                                        No commission
+                                    </Typography>
+                                    <Typography
+                                        sx={{
+                                            color: "#fff",
+                                            fontSize: "13px",
+                                            marginBottom: "5px",
+                                        }}
+                                    >
+                                        <HourglassEmptyIcon
+                                            sx={{ color: "#009eff" }}
+                                        />{" "}
+                                        Quick withdrawals
+                                    </Typography>
+                                    <Typography
+                                        sx={{
+                                            color: "#fff",
+                                            fontSize: "13px",
+                                            marginBottom: "5px",
+                                        }}
+                                    >
+                                        <CreditCardIcon
+                                            sx={{ color: "#009eff" }}
+                                        />{" "}
+                                        Minimum withdrawal amount: $10
+                                    </Typography>
+                                </Box>
+                            </div>
                         </div>
                     </Box>
                 </Box>
