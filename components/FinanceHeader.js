0a2b66d7da57bf03aac6076b937252d982e67a6c
haruntahid <tahidtaha997@gmail.com>
2022-12-09 02:37:46 +0600

    mysafe

--- components/FinanceHeader.js
+++ components/FinanceHeader.js
@@ -9,6 +9,7 @@ import Withdrawal from "./Withdrawal";
 import Deposit from "./Deposit";
 import PromoCode from "./PromoCode";
 import Cashback from "./Cashback";
+import MySafe from "./MySafe";
 import BottomNavigation from "@mui/material/BottomNavigation";
 import BottomNavigationAction from "@mui/material/BottomNavigationAction";
 import RestoreIcon from "@mui/icons-material/Restore";
@@ -17,170 +18,159 @@ import ArchiveIcon from "@mui/icons-material/Archive";
 import Paper from "@mui/material/Paper";
 
 function TabPanel(props) {
-    const { children, value, index, ...other } = props;
+  const { children, value, index, ...other } = props;
 
-    return (
-        <div
-            role="tabpanel"
-            hidden={value !== index}
-            id={`simple-tabpanel-${index}`}
-            aria-labelledby={`simple-tab-${index}`}
-            {...other}
-        >
-            {value === index && (
-                <Box sx={{ width: "100%", backgroundColor: "#131628" }}>
-                    {children}
-                </Box>
-            )}
-        </div>
-    );
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ width: "100%", backgroundColor: "#131628" }}>{children}</Box>
+      )}
+    </div>
+  );
 }
 
 TabPanel.propTypes = {
-    children: PropTypes.node,
-    index: PropTypes.number.isRequired,
-    value: PropTypes.number.isRequired,
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
 };
 
 function FinanceHeader() {
-    const [value, setValue] = React.useState(0);
+  const [value, setValue] = React.useState(0);
 
-    const handleChange = (event, newValue) => {
-        setValue(newValue);
-    };
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
 
-    const router = useRouter();
+  const router = useRouter();
 
-    return (
-        <>
-            <Box
-                sx={{
-                    display: "block",
-                    backgroundColor: "#202237",
-                    width: "100%",
-                    height: "calc(100vh - 60px)",
-                }}
-            >
-                {" "}
-                <Box
-                    sx={{
-                        borderBottom: 1,
-                        borderColor: "divider",
-                    }}
-                >
-                    <Tabs
-                        value={value}
-                        onChange={handleChange}
-                        variant="scrollable"
-                        scrollButtons
-                        allowScrollButtonsMobile
-                        sx={{
-                            width: "100%",
-                            "@media(max-width:900px)": {
-                                display: "none",
-                            },
-                        }}
-                    >
-                        <Tab
-                            label="DEPOSIT"
-                            onClick={(event) => {
-                                event.preventDefault();
-                                router.push("/finance/deposit");
-                            }}
-                        />
-                        <Tab
-                            label="WITHDRAWAL"
-                            onClick={(event) => {
-                                event.preventDefault();
-                                router.push("/finance/withdrawal");
-                            }}
-                        />
-                        <Tab
-                            label="HISTORY"
-                            onClick={(event) => {
-                                event.preventDefault();
-                                router.push("/finance/history");
-                            }}
-                        />
-                        <Tab
-                            label="cashback"
-                            onClick={(event) => {
-                                event.preventDefault();
-                                router.push("/finance/cashback");
-                            }}
-                        />
-                        <Tab
-                            label="promo codes"
-                            onClick={(event) => {
-                                event.preventDefault();
-                                router.push("/finance/promo");
-                            }}
-                        />
-                        <Tab
-                            label="my safe"
-                            onClick={(event) => {
-                                event.preventDefault();
-                                router.push("/finance/my-safe");
-                            }}
-                        />
-                    </Tabs>
-                </Box>
-                <TabPanel value={value} index={0}>
-                    <Deposit />
-                </TabPanel>
-                <TabPanel value={value} index={1}>
-                    <Withdrawal />
-                </TabPanel>
-                <TabPanel value={value} index={2}>
-                    item3
-                </TabPanel>
-                <TabPanel value={value} index={3}>
-                    <Cashback />
-                </TabPanel>
-                <TabPanel value={value} index={4}>
-                    <PromoCode />
-                </TabPanel>
-                <TabPanel value={value} index={5}>
-                    Item 6
-                </TabPanel>
-            </Box>
-            <Paper
-                sx={{
-                    backgroundColor: "#202434",
-                    position: "fixed",
-                    bottom: 0,
-                    left: 0,
-                    right: 0,
-                    display: "none",
-                    "@media(max-width:900px)": {
-                        display: "block",
-                    },
-                }}
-                elevation={3}
-            >
-                <BottomNavigation
-                    showLabels
-                    value={value}
-                    onChange={(event, newValue) => {
-                        setValue(newValue);
-                    }}
-                >
-                    <BottomNavigationAction
-                        label="Recents"
-                        icon={<RestoreIcon />}
-                    />
-                    <BottomNavigationAction
-                        label="Favorites"
-                        icon={<FavoriteIcon />}
-                    />
-                    <BottomNavigationAction
-                        label="Archive"
-                        icon={<ArchiveIcon />}
-                    />
-                </BottomNavigation>
-            </Paper>
-        </>
-    );
+  return (
+    <>
+      <Box
+        sx={{
+          display: "block",
+          backgroundColor: "#202237",
+          width: "100%",
+          height: "calc(100vh - 60px)",
+        }}
+      >
+        {" "}
+        <Box
+          sx={{
+            borderBottom: 1,
+            borderColor: "divider",
+          }}
+        >
+          <Tabs
+            value={value}
+            onChange={handleChange}
+            variant="scrollable"
+            scrollButtons
+            allowScrollButtonsMobile
+            sx={{
+              width: "100%",
+              "@media(max-width:900px)": {
+                display: "none",
+              },
+            }}
+          >
+            <Tab
+              label="DEPOSIT"
+              onClick={(event) => {
+                event.preventDefault();
+                router.push("/finance/deposit");
+              }}
+            />
+            <Tab
+              label="WITHDRAWAL"
+              onClick={(event) => {
+                event.preventDefault();
+                router.push("/finance/withdrawal");
+              }}
+            />
+            <Tab
+              label="HISTORY"
+              onClick={(event) => {
+                event.preventDefault();
+                router.push("/finance/history");
+              }}
+            />
+            <Tab
+              label="cashback"
+              onClick={(event) => {
+                event.preventDefault();
+                router.push("/finance/cashback");
+              }}
+            />
+            <Tab
+              label="promo codes"
+              onClick={(event) => {
+                event.preventDefault();
+                router.push("/finance/promo");
+              }}
+            />
+            <Tab
+              label="my safe"
+              onClick={(event) => {
+                event.preventDefault();
+                router.push("/finance/my-safe");
+              }}
+            />
+          </Tabs>
+        </Box>
+        <TabPanel value={value} index={0}>
+          <Deposit />
+        </TabPanel>
+        <TabPanel value={value} index={1}>
+          <Withdrawal />
+        </TabPanel>
+        <TabPanel value={value} index={2}>
+          item3
+        </TabPanel>
+        <TabPanel value={value} index={3}>
+          <Cashback />
+        </TabPanel>
+        <TabPanel value={value} index={4}>
+          <PromoCode />
+        </TabPanel>
+        <TabPanel value={value} index={5}>
+          <MySafe />
+        </TabPanel>
+      </Box>
+      <Paper
+        sx={{
+          backgroundColor: "#202434",
+          position: "fixed",
+          bottom: 0,
+          left: 0,
+          right: 0,
+          display: "none",
+          "@media(max-width:900px)": {
+            display: "block",
+          },
+        }}
+        elevation={3}
+      >
+        <BottomNavigation
+          showLabels
+          value={value}
+          onChange={(event, newValue) => {
+            setValue(newValue);
+          }}
+        >
+          <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
+          <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
+          <BottomNavigationAction label="Archive" icon={<ArchiveIcon />} />
+        </BottomNavigation>
+      </Paper>
+    </>
+  );
 }
 
 export default FinanceHeader;
---
+++ components/MySafe.js
@@ -0,0 +1,409 @@
+import React from "react";
+import { Box, Typography } from "@mui/material";
+import TextSnippetIcon from "@mui/icons-material/TextSnippet";
+import OpenInNewIcon from "@mui/icons-material/OpenInNew";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import DemoLine from "./Chart";
+import DemoColumn from "./Chart-2";
+
+function MySafe() {
+  const [age, setAge] = React.useState("");
+
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
+  return (
+    <>
+      <Box
+        sx={{
+          padding: "15px",
+          height: "calc(100vh - 108px)",
+          overflowY: "scroll",
+          overflowX: "hidden",
+          width: "100%",
+        }}
+      >
+        <Typography
+          sx={{ fontSize: "24px", marginBottom: "14px", color: "#fff" }}
+        >
+          My Safe
+        </Typography>
+        <Typography
+          sx={{
+            maxWidth: "1200px",
+            color: "#fff",
+            fontSize: "14px",
+            marginBottom: "19px",
+          }}
+        >
+          Open your personal safe for savings and receive passive income of up
+          to <span style={{ fontWeight: "700" }}>10%</span> per annum! <br />{" "}
+          Receive profit from your financial assets currently not used in
+          trading for a truly passive source of income with Pocket Option.
+          Deposits and withdrawals are available at any time without any
+          restrictions or commissions.
+        </Typography>
+        <Typography
+          sx={{
+            maxWidth: "1200px",
+            color: "#fff",
+            fontSize: "14px",
+            marginBottom: "19px",
+          }}
+        >
+          Cooperation with major European and Asian banks opens up opportunities
+          for investing in government bonds with a fixed guaranteed income and
+          promising investment funds secured by demand deposits. Check out the
+          full terms of use below.
+        </Typography>
+        <Box>
+          <a
+            href="http://"
+            target="_blank"
+            style={{
+              color: "#8ea5c0",
+              display: "flex",
+              alignItems: "center",
+              marginBottom: "32px",
+            }}
+          >
+            <TextSnippetIcon sx={{ marginRight: "7px" }} />
+            Terms of use{" "}
+            <span style={{ fontSize: "13px" }}>
+              [ Pdf, 327 kb <OpenInNewIcon sx={{ fontSize: "14px" }} /> ]
+            </span>
+          </a>
+        </Box>
+
+        <Box style={{ maxWidth: "1200px" }}>
+          <Typography
+            sx={{
+              fontSize: "20px",
+              fontWeight: "700",
+              color: "#fff",
+              marginTop: "10px",
+              marginBottom: "4px",
+            }}
+          >
+            Choose a Safe type
+          </Typography>
+
+          {/* boxs */}
+
+          <Box className="row">
+            <Box
+              className="col-md-4"
+              sx={{
+                background: "#161a2b",
+                padding: "10px 25px 30px",
+                textAlign: "center",
+                transition: ".5s",
+                opacity: ".2",
+                borderRadius: "5px",
+                padding: "0 20px",
+                "&:hover": {
+                  background: "#111424",
+                  opacity: "1",
+                },
+              }}
+            >
+              <img
+                src="https://pocketoption.com/themes/cabinet/images/pages/my-safe/tab/1.png"
+                alt=""
+              />
+              <Typography
+                sx={{
+                  fontSize: "20px",
+                  fontWeight: "700",
+                  color: "#fff",
+                  marginTop: "10px",
+                }}
+              >
+                USD Safe
+              </Typography>
+            </Box>
+            <Box
+              className="col-md-4"
+              sx={{
+                background: "#161a2b",
+                padding: "10px 25px 30px",
+                textAlign: "center",
+                transition: ".5s",
+                opacity: ".2",
+                borderRadius: "5px",
+                "&:hover": {
+                  background: "#111424",
+                  opacity: "1",
+                },
+              }}
+            >
+              <img
+                src="https://pocketoption.com/themes/cabinet/images/pages/my-safe/tab/2.png"
+                alt=""
+              />
+              <Typography
+                sx={{
+                  fontSize: "20px",
+                  fontWeight: "700",
+                  color: "#fff",
+                  marginTop: "10px",
+                }}
+              >
+                Bitcoin Safe
+              </Typography>
+            </Box>
+            <Box
+              className="col-md-4"
+              sx={{
+                background: "#161a2b",
+                padding: "10px 25px 30px",
+                textAlign: "center",
+                transition: ".5s",
+                opacity: ".2",
+                borderRadius: "5px",
+                "&:hover": {
+                  background: "#111424",
+                  opacity: "1",
+                },
+              }}
+            >
+              <img
+                src="https://pocketoption.com/themes/cabinet/images/pages/my-safe/tab/3.png"
+                alt=""
+              />
+              <Typography
+                sx={{
+                  fontSize: "20px",
+                  fontWeight: "700",
+                  color: "#fff",
+                  marginTop: "10px",
+                }}
+              >
+                Ethereum Safe
+              </Typography>
+            </Box>
+          </Box>
+
+          <Typography
+            sx={{
+              fontSize: "20px",
+              fontWeight: "700",
+              color: "#fff",
+              marginTop: "10px",
+              marginBottom: "4px",
+            }}
+          >
+            Сalculate the profitability of your safe
+          </Typography>
+
+          <Box className="row" sx={{ marginTop: "15px" }}>
+            <Box className="col-md-8">
+              <Box className="row">
+                <Box className="col-md-6">
+                  <Typography
+                    sx={{
+                      color: "#7e91a7",
+                      fontWeight: "700",
+                      fontSize: "16px",
+                    }}
+                  >
+                    Choose a safe
+                  </Typography>
+                  <FormControl sx={{ m: 1, width: "100%" }} size="small">
+                    <Select
+                      labelId="demo-select-small"
+                      id="demo-select-small"
+                      value={age}
+                      label="Age"
+                      onChange={handleChange}
+                    >
+                      <MenuItem value={10}>10%</MenuItem>
+                      <MenuItem value={20}>7%</MenuItem>
+                      <MenuItem value={30}>5%</MenuItem>
+                      <MenuItem value={40}>3%</MenuItem>
+                    </Select>
+                  </FormControl>
+                </Box>
+                <Box className="col-md-6">
+                  <Typography
+                    sx={{
+                      color: "#7e91a7",
+                      fontWeight: "700",
+                      fontSize: "16px",
+                    }}
+                  >
+                    Duration
+                  </Typography>
+                  <FormControl sx={{ m: 1, width: "100%" }} size="small">
+                    <Select
+                      labelId="demo-select-small"
+                      id="demo-select-small"
+                      value={age}
+                      label="Age"
+                      onChange={handleChange}
+                    >
+                      <MenuItem value={10}>10%</MenuItem>
+                      <MenuItem value={20}>7%</MenuItem>
+                      <MenuItem value={30}>5%</MenuItem>
+                      <MenuItem value={40}>3%</MenuItem>
+                    </Select>
+                  </FormControl>
+                </Box>
+              </Box>
+              <Box className="row">
+                <Box className="col-md-6">
+                  <Typography
+                    sx={{
+                      color: "#7e91a7",
+                      fontWeight: "700",
+                      fontSize: "16px",
+                    }}
+                  >
+                    Choose a safe
+                  </Typography>
+                  <FormControl sx={{ m: 1, width: "100%" }} size="small">
+                    <Select
+                      labelId="demo-select-small"
+                      id="demo-select-small"
+                      value={age}
+                      label="Age"
+                      onChange={handleChange}
+                    >
+                      <MenuItem value={10}>10%</MenuItem>
+                      <MenuItem value={20}>7%</MenuItem>
+                      <MenuItem value={30}>5%</MenuItem>
+                      <MenuItem value={40}>3%</MenuItem>
+                    </Select>
+                  </FormControl>
+                </Box>
+                <Box className="col-md-6">
+                  <Typography
+                    sx={{
+                      color: "#7e91a7",
+                      fontWeight: "700",
+                      fontSize: "16px",
+                    }}
+                  >
+                    Duration
+                  </Typography>
+                  <FormControl sx={{ m: 1, width: "100%" }} size="small">
+                    <Select
+                      labelId="demo-select-small"
+                      id="demo-select-small"
+                      value={age}
+                      label="Age"
+                      onChange={handleChange}
+                    >
+                      <MenuItem value={10}>10%</MenuItem>
+                      <MenuItem value={20}>7%</MenuItem>
+                      <MenuItem value={30}>5%</MenuItem>
+                      <MenuItem value={40}>3%</MenuItem>
+                    </Select>
+                  </FormControl>
+                </Box>
+              </Box>
+            </Box>
+            <Box className="col-md-4">
+              <Typography
+                sx={{
+                  color: "#7e91a7",
+                  fontWeight: "700",
+                  fontSize: "16px",
+                }}
+              >
+                Calculation results on 2024-01-08
+              </Typography>
+              <Box
+                sx={{
+                  backgroungColor: "#161a2b",
+                  border: "1px solid #20273f",
+                  padding: "20px",
+                  borderRadius: "5px",
+                }}
+              >
+                <Typography
+                  sx={{ fontSize: "14px", marginBottom: "10px", color: "#fff" }}
+                >
+                  Total income
+                </Typography>
+                <Typography
+                  sx={{
+                    fontSize: "40px",
+                    paddingBottom: "15px",
+                    color: "#F0B90B",
+                    fontWeight: "700",
+                  }}
+                >
+                  $6257
+                </Typography>
+                <Typography
+                  sx={{ fontSize: "14px", marginBottom: "10px", color: "#fff" }}
+                >
+                  Final safe balance
+                </Typography>
+                <Typography
+                  sx={{
+                    fontSize: "40px",
+                    color: "#F0B90B",
+                    fontWeight: "700",
+                  }}
+                >
+                  $69257
+                </Typography>
+              </Box>
+            </Box>
+          </Box>
+          <Box className="row">
+            <Box className="col-md-6">
+              <Typography
+                sx={{
+                  color: "#7e91a7",
+                  fontWeight: "700",
+                  fontSize: "16px",
+                }}
+              >
+                Balance chart
+              </Typography>
+              <DemoLine
+                sx={{
+                  backgroungColor: "#161a2b",
+                  border: "1px solid #20273",
+                  minWidth: "500px",
+                  padding: "0 30px",
+                }}
+              />
+            </Box>
+            <Box className="col-md-6">
+              <Typography
+                sx={{
+                  color: "#7e91a7",
+                  fontWeight: "700",
+                  fontSize: "16px",
+                }}
+              >
+                Income chart
+              </Typography>
+              <DemoColumn />
+            </Box>
+          </Box>
+          <Typography
+            sx={{
+              fontSize: "12px",
+              fontStyle: "italic",
+              marginTop: "20px",
+              color: "#fff",
+            }}
+          >
+            * The monthly calculation of safe revenue is based on daily payouts
+            and may vary depending on the number of days in a month
+          </Typography>
+        </Box>
+      </Box>
+    </>
+  );
+}
+
+export default MySafe;
