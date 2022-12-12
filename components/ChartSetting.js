3bb8765d8a0a05fba5bd668d03d7e28f05f9d32d
Nazmul <Nazmul.abbu@gmail.com>
2022-12-13 02:27:06 +0600

    helo

--- components/ChartSetting.js
+++ components/ChartSetting.js
@@ -11,7 +11,6 @@ function ChartSetting() {
                     left: "0",
                     top: "0",
                     zIndex: "111",
-                    background: "red",
                 }}
             >
                 Hello
