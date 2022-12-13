79e08ec1822890865940a939ec463edc301a5846
Nazmul <Nazmul.abbu@gmail.com>
2022-12-14 02:50:39 +0600

    list

--- components/ChartSetting.js
+++ components/ChartSetting.js
@@ -2,9 +2,18 @@ import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
-import React from "react";
+import React, { useState } from "react";
 import SettingsIcon from "@mui/icons-material/Settings";
 import ChartOptions from "./ChartOptions";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import CurrencyBitcoinIcon from "@mui/icons-material/CurrencyBitcoin";
+import { FaEthereum } from "react-icons/fa";
 const style = {
     position: "absolute",
     top: "50%",
@@ -18,6 +27,7 @@ const style = {
 };
 
 function ChartSetting() {
+    const [charttop, setCharttop] = useState("Bitcoin");
     const [open, setOpen] = React.useState(false);
     const [open2, setOpen2] = React.useState(false);
     const handleOpen = () => setOpen(true);
@@ -44,7 +54,7 @@ function ChartSetting() {
                         scale: "0.9",
                     }}
                 >
-                    Bitcoin
+                    {charttop}
                 </Button>
                 <Button
                     size="small"
@@ -64,7 +74,64 @@ function ChartSetting() {
                 aria-labelledby="modal-modal-title"
                 aria-describedby="modal-modal-description"
             >
-                <Box sx={style}>coming from server</Box>
+                <Box sx={style}>
+                    <Box
+                        sx={{
+                            color: "#9396a0",
+                            display: "flex",
+                            justifyContent: "space-between",
+                        }}
+                    >
+                        <Typography fontSize={14}>Crypto currencies</Typography>
+                        <Typography fontSize={14}>
+                            Payout <ArrowDropDownIcon />
+                        </Typography>
+                    </Box>
+                    <List>
+                        <ListItem disablePadding>
+                            <ListItemButton
+                                onClick={() => {
+                                    setCharttop("Bitcoin");
+                                    handleClose();
+                                }}
+                            >
+                                <ListItemIcon sx={{ color: "#fff" }}>
+                                    <CurrencyBitcoinIcon />
+                                </ListItemIcon>
+                                <ListItemText
+                                    sx={{ color: "#fff" }}
+                                    secondary="Bitcoin"
+                                />
+                                <ListItemText
+                                    secondary="40%"
+                                    sx={{ textAlign: "right" }}
+                                />
+                            </ListItemButton>
+                        </ListItem>
+                        <ListItem disablePadding>
+                            <ListItemButton
+                                onClick={() => {
+                                    setCharttop("Ethereum");
+                                    handleClose();
+                                }}
+                            >
+                                <ListItemIcon
+                                    sx={{ color: "#fff", padding: "5px" }}
+                                >
+                                    <FaEthereum />
+                                </ListItemIcon>
+                                <ListItemText
+                                    sx={{ color: "#fff" }}
+                                    secondary="Ethereum"
+                                />
+                                <ListItemText
+                                    secondary="50%"
+                                    sx={{ textAlign: "right" }}
+                                />{" "}
+                            </ListItemButton>
+                        </ListItem>
+                    </List>
+                </Box>
             </Modal>
             <Modal
                 open={open2}
---
+++ components/ChartTopic.js
@@ -0,0 +1,8 @@
+import { Box, Typography } from "@mui/material";
+import React from "react";
+
+function ChartTopic() {
+    return <></>;
+}
+
+export default ChartTopic;
